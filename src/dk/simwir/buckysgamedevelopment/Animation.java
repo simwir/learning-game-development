ef2674d4a0d93e3974420f94a99cd5365155c528
Simon <simwir@hotmail.com>
2014-11-24 20:25:52 +0100

    Done with video 13. Screen class is going away being replaced by Screen manager

--- src/dk/simwir/buckysgamedevelopment/Animation.java
+++ src/dk/simwir/buckysgamedevelopment/Animation.java
@@ -1,5 +1,6 @@
 package dk.simwir.buckysgamedevelopment;
 
+import java.awt.Image;
 import java.util.ArrayList;
 
 public class Animation {
@@ -8,16 +9,70 @@ public class Animation {
 	private int sceneIndex;
 	private long movieTime, totalTime;
 	
-	private Animation(){
+	public Animation(){
 		scenes = new ArrayList();
 		totalTime = 0;
 		start();
 	}
 	
+	
+
 	//add scene to array list and set time for each scene
 	public synchronized void addScene(Image image, long time){
 		totalTime += time;
 		scenes.add(new OneScene(image, totalTime));
+	}
+	
+	//Start animation from the beginning
+	public synchronized void start() {
+		movieTime = 0;
+		sceneIndex = 0;
+	}
+	
+	//changes scenes
+	public synchronized void update(long timePassed){
+		if(scenes.size()>1){
+			movieTime += timePassed;
+			
+			/*
+			 * if the animation loop is done, but there is more time left
+			 * the animation should be running, this resets it.
+			 */
+			if(movieTime >= totalTime){
+				movieTime = 0;
+				sceneIndex = 0;
+			}
+			while(movieTime > getScene(sceneIndex).endTime){
+				sceneIndex++;
+			}
+		}
+	}
+	
+	//get animation current image
+	public synchronized Image getImage(){
+		if(scenes.size()==0){
+			return null;
+		}else{
+			return getScene(sceneIndex).pic;
+		}
+	}
+
+
+
+	private OneScene getScene(int x) {
+		return (OneScene) scenes.get(x);
+	}
+	
+///////////// PRIVATE INNER CLASS//////////
+	
+	private class OneScene{
+		Image pic;
+		long endTime;
+		
+		public OneScene(Image pic, long endTime){
+			this.pic = pic;
+			this.endTime = endTime;
+		}
 	}
 	
 }
--- src/dk/simwir/buckysgamedevelopment/Bucky.java
+++ src/dk/simwir/buckysgamedevelopment/Bucky.java
@@ -1,49 +1,87 @@
 package dk.simwir.buckysgamedevelopment;
 
-import java.awt.Color;
 import java.awt.DisplayMode;
-import java.awt.Font;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.RenderingHints;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-/*
- * Video 7
- */
-public class Bucky extends JFrame{
+
+public class Bucky extends JFrame {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3083912055609240770L;
 
 	public static void main(String[] args) {
-		
-		DisplayMode dm = new DisplayMode(800, 600, 16, DisplayMode.REFRESH_RATE_UNKNOWN);
+
+		DisplayMode dispayMode = new DisplayMode(800, 600, 16,
+				DisplayMode.REFRESH_RATE_UNKNOWN);
 		Bucky b = new Bucky();
-		b.run(dm);
+		b.run(dispayMode);
 	}
 
+	private Screen screen;
+	private Image backGround;
+	private Animation animation;
+
+	// loads pictures from computer to jave and adds them to a scene
+	public void loadPics() {
+		backGround = new ImageIcon(
+				"C:\\Users\\Simon\\Documents\\java\\Buckys game development\\resources\\back.jpg")
+				.getImage();
+		Image face1 = new ImageIcon(
+				"C:\\Users\\Simon\\Documents\\java\\Buckys game development\\resources\\face.png")
+				.getImage();
+		Image face2 = new ImageIcon(
+				"C:\\Users\\Simon\\Documents\\java\\Buckys game development\\resources\\face2.png")
+				.getImage();
+		animation = new Animation();
+		animation.addScene(face1, 250);
+		animation.addScene(face2, 250);
+	}
+
+	// main engine run
 	public void run(DisplayMode dm) {
-		setBackground(Color.BLUE);
-		setForeground(Color.WHITE);
-		setFont(new Font("Arial", Font.PLAIN, 24));
-		
-		Screen s = new Screen();
-		
-		try{
-			s.setFullScreen(dm, this);
-			try {
-				Thread.sleep(3000);
-			} catch (Exception e) {}
-		}finally{
-			s.restoreScreen();
+		screen = new Screen();
+		try {
+			screen.setFullScreen(dm, new JFrame());
+			loadPics();
+			movieLoop();
+		} finally {
+			screen.restoreScreen();
 		}
 	}
-	
-	public void paint(Graphics g){
-		if(g instanceof Graphics2D){
-			Graphics2D g2 = (Graphics2D) g;
-			g2.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-			
+
+	// main movie loop
+	public void movieLoop() {
+		long startingTime = System.currentTimeMillis();
+		long cumTime = startingTime;
+
+		while (cumTime - startingTime < 5000) {
+			long timePassed = System.currentTimeMillis() - cumTime;
+			cumTime += timePassed;
+			animation.update(timePassed);
+
+			Graphics g = screen.getFullScreenWindow().getGraphics();
+			draw(g);
+			g.dispose();
+
+			try {
+				Thread.sleep(20);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+
 		}
-		g.drawString("Test", 200, 200);
+
+	}
+
+	// draw method
+	public void draw(Graphics g) {
+		g.drawImage(backGround, 0, 0, null);
+		g.drawImage(animation.getImage(), 0, 0, null);
 	}
 
 }
